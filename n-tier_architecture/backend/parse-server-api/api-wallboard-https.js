d2e78012ae47841e3d0b63edb446654d5b163470
“PraDuke <poomodify@gmail.com>
2025-03-15 15:41:39 +0700

    15/03

--- n-tier_architecture/backend/parse-server-api/api-wallboard-https.js
+++ n-tier_architecture/backend/parse-server-api/api-wallboard-https.js
@@ -12,43 +12,52 @@ const databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 var apiport = 4000;
 
-const config = {
-  databaseURI: 'mongodb://wallboard:wallboard1q2w3e4r@127.0.0.1:27017/wallboarddb',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'wallboardapi',
-  masterKey: process.env.MASTER_KEY || 'wallboardapi', //Add your master key here. Keep it secret!
-  clientKey: 'wallboardapi',
-  javascriptKey: 'wallboardapi',
-  serverURL: 'https://192.168.56.10:' + apiport + '/api', // Don't forget to change to https if needed
-  publicServerURL: 'https://192.168.56.10:' + apiport + '/api',
-  liveQuery: {
-    classNames: ['OnlineAgentLists', 'WallboardBanners','CallAgentSummaries'], // List of classes to support for query subscriptions
-  },
-};
-
-const app = express();
-
-app.use(cors());
-app.use(cors({ origin: '*' }))
-
-// Serve static assets from the /public folder
-app.use('/', express.static(path.join(__dirname, '/wallboard')));
-
-// Serve the Parse API on the /parse URL prefix
-const mountPath = '/api';
-const api = new ParseServer(config);
-
 var options = {
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.crt')
 };
 
-app.use(mountPath, api);
+const config = { 
+    databaseURI: 'mongodb://wallboarduser:WB1qazxsw2@localhost:27017/wallboarddb',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'wallboardapi',
+    masterKey: process.env.MASTER_KEY || 'wallboardapi', //Add your master key here. Keep it secret!
+    clientKey: 'wallboardapi',
+    javascriptKey: 'wallboardapi',
+    serverURL: 'https://localhost:' + apiport + '/api', // Don't forget to change to https if needed
+    publicServerURL: 'https://localhost:' + apiport + '/api',
+    liveQuery: {
+      classNames: ['OnlineAgentLists', 'WallboardBanners','CallAgentSummaries'], // List of classes to support for query subscriptions
+    },
+    //masterKeyIps: ['0.0.0.0/0'],
+    masterKeyIps: ["0.0.0.0/0", "::/0"],
+    useMasterKey: true,
+    allowClientClassCreation: false,
+    allowExpiredAuthDataToken: false,
+  //  encodeParseObjectInCloudFunction: false 
+  };
 
-var httpsServer = require('https').createServer(options, app);
+  const app = express();
 
-httpsServer.listen(apiport, function () {
-  console.log('Wallboard API running on port ' + apiport + '.');
-});
+  app.use(cors());
+  app.use(cors({ origin: '*'}))
 
-ParseServer.createLiveQueryServer(httpsServer);
+  // Serve static assets from the /public folder
+  app.use('/', express.static(path.join(__dirname, '/wallboard')));
+
+  // Serve the Parse API on the /parse URL prefix
+  const mountPath = '/api';
+  const api = new ParseServer(config);
+  
+  // 3. Start up Parse Server asynchronously
+   api.start();
+
+  app.use(mountPath, api.app);
+
+  var httpsServer = require('https').createServer(options, app);
+
+  httpsServer.listen(apiport, function() {   
+     console.log('3CX Wallboard API (htts) running on port ' + apiport + '.');
+  });
+
+ ParseServer.createLiveQueryServer(httpsServer);
